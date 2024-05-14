2193b564198cfbdebd2106a477ae71fe70400b07
Insung Cho <bfdev@icloud.com>
2024-05-14 14:17:10 +0800

    feat: include score to description

--- main.ts
+++ main.ts
@@ -25,7 +25,7 @@ function eventToIcs(event: EsportEvent): Event | null {
     title: `${event.match?.teams[0].code} vs ${event.match?.teams[1].code}`,
     beginDate: date,
     endDate: addHoursToDate(date, event.match.strategy.count * 0.5),
-    desc: `${event.league.name} ${event.blockName}: ${event.match?.teams[0].name} vs ${event.match?.teams[1].name}`,
+    desc: `${event.league.name} ${event.blockName}: ${event.match?.teams[0].name} ${event.match.teams[0].result.gameWins} : ${event.match.teams[0].result.gameWins} ${event.match?.teams[1].name}`,
     zone: "utc",
   });
 }
